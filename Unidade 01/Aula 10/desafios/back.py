7ac3cc93131dd0712a803e03d70934808a560ccf
ムドジム <mauricioboy14g@gmail.com>
2026-08-10 20:38:12 -0300

    Atualização

--- Unidade 01/Aula 10/desafios/back.py
+++ Unidade 01/Aula 10/desafios/back.py
@@ -1,35 +1,87 @@
-# Crie um estoque de produtos funcional em Python
-# para um mercado ultilizando Dicionários.
+import plyer
 
-# Crie uma lista de estoque.
-# Os produtos serão um dicionário com TIPO, PREÇO UNITÁRIO, QUANTIDADE.
-# Devemos poder ACESSAR e CRIAR produtos.
-# Crie funções para obtermos a SOMA TOTAL de preços e a SOMA TOTAL DE QUANTIDADES.
+estoque = []
 
-estoque = [
-    {"tipo":"arroz","preço":"20","quantidade":"10"},
-    {"tipo":"feijão","preço":"10","quantidade":"15"},
-    {"tipo":"macarrão","preço":"7","quantidade":"20"},
-    {"tipo":"farofa","preço":"5","quantidade":"30"},
-    {"tipo":"carne","preço":"80","quantidade":"35"},
-    {"tipo":"sal","preço":"2","quantidade":"25"},
-]
+produto = {
+    "tipo": "Café",
+    "preco_unitario": 28.99,
+    "quantidade": 300
+}
+
+def buscar_produto():
+    busca = input("Digite o produto buscado")
+
+    for produto in estoque:
+        if produto["tipo"] == busca:
+            print(produto["quantidade"])
+            print(produto["preco_unitario"])
+
+def adicionar_produtos():
+    try:
+        produto = {
+            "tipo": input("Digite o tipo de produto: "),
+            "preco_unitario": float(input("Digite o preço unitário: ")),
+            "quantidade": int(input("Digite a quantidade: "))
+        }
+        estoque.append(produto)
+        print("Produto adicionado corretamente.")
+
+    except:
+        print("Algo deu errado. Tente novamente.")
+
+def listar_produtos():
+    for produto in estoque:
+        print(produto)
+
+def somar_total():
+    total_geral = 0
+
+    for produto in estoque:
+        total_geral += produto["preco_unitario"] * produto["quantidade"]
+
+    print(f"Total geral do estoque: {total_geral}")
+
+def somar_quantidade_total():
+    total_geral = 0
+
+    for produto in estoque:
+        total_geral += produto["quantidade"]
+
+    for produto in estoque:
+        frase = produto["tipo"] + " - " + str(produto["quantidade"])
+        print(frase)
+
+    print(f"Total geral do estoque: {total_geral}")
+
+# Crie uma função que, quando executada, exiba todos os produtos
+# que estão com estoque abaixo do mínimo (abaixo de 50).
+
+def limite_estoque():
+    for produto in estoque:
+        if produto["quantidade"] < 50:
+            plyer.notification.notify(
+                title = "Falta de orgamento.",
+                message = f"Atenção! O patrão está sem grana.",
+                app_name = "Sistema de Estoque",
+                timeout = 5
+            )
+        else:
+            print("Todos acima do limite!")
+    
+
 
 while True:
-    escolha_usuario = int(input("Menu\n1-Lista\n2-Acrescentar\n3-Valor\n4-Estoque\n0-Sair\n:"))
+    opcao = input("1- Adicionar produto\n2- Listar produtos\n3- Preço Total\n4- Quantidade Total\n5- Limite estoque\n: ")
 
-    if escolha_usuario == 1:
-        for chave in estoque:
-                print(chave)
-
-    elif escolha_usuario == 2:
-            produto = {
-                "tipo":input("Digite o nome do produto: "),
-                "valor":input("Digite o valor do produto: "),
-                "quantidade":input("Digite a quantidade do produto: ")
-            }
-            estoque.append(produto)
-
-    elif escolha_usuario == 0:
-         print("Obrigado por usar o nosso sistema.")
-         break
+    if opcao == "1":
+        adicionar_produtos()
+    elif opcao == "2":
+        listar_produtos()
+    elif opcao == "3":
+        somar_total()
+    elif opcao == "4":
+        somar_quantidade_total()
+    elif opcao == "5":
+        limite_estoque()
+    else:
+        print("Digite uma opção válida.")
--- Unidade 01/Aula 10/desafios/desafio01.py
+++ Unidade 01/Aula 10/desafios/desafio01.py
@@ -1,55 +1,61 @@
-# Crie um estoque de produtos funcional em Python
-# para um mercado ultilizando Dicionários.
-
-# Crie uma lista de estoque.
-# Os produtos serão um dicionário com TIPO, PREÇO UNITÁRIO, QUANTIDADE.
-# Devemos poder ACESSAR e CRIAR produtos.
-# Crie funções para obtermos a SOMA TOTAL de preços e a SOMA TOTAL DE QUANTIDADES.
-
 estoque = [
-    {"tipo":"arroz","preço":"20","quantidade":"10"},
-    {"tipo":"feijão","preço":"10","quantidade":"15"},
-    {"tipo":"macarrão","preço":"7","quantidade":"20"},
-    {"tipo":"farofa","preço":"5","quantidade":"30"},
-    {"tipo":"carne","preço":"80","quantidade":"35"},
-    {"tipo":"sal","preço":"2","quantidade":"25"},
+    {"tipo": "arroz", "preço": 20, "quantidade": 10},
+    {"tipo": "feijão", "preço": 10, "quantidade": 15},
+    {"tipo": "macarrão", "preço": 7, "quantidade": 20},
+    {"tipo": "farofa", "preço": 5, "quantidade": 30},
+    {"tipo": "carne", "preço": 80, "quantidade": 35},
+    {"tipo": "sal", "preço": 2, "quantidade": 25},
 ]
 
 while True:
-    escolha_usuario = int(input("Menu\n1-Estoque\n2-acrescentar\n3-Valor\n4-Estoque\n0-Sair\n: "))
+    escolha_usuario = int(input("Menu\n1- Lista\n2- Acrescentar\n3- Valor\n4- Estoque\n5- Buscar Produto\n0- Sair\n: "))
 
     if escolha_usuario == 1:
         for chave in estoque:
-                print(chave)
+            print(chave)
 
     elif escolha_usuario == 2:
+        try:
             produto = {
-                "tipo":input("Digite o nome do produto: "),
-                "valor":input("Digite o valor do produto: "),
-                "quantidade":input("Digite a quantidade do produto: ")
+                "tipo": input("Digite o nome do produto: "),
+                "preço": float(input("Digite o valor do produto: ")),
+                "quantidade": int(input("Digite a quantidade do produto: "))
             }
-            estoque.append(produto)
 
-    
+            estoque.append(produto)
+            print("Produto adicionado com sucesso.")
+
+        except:
+            print("Algo deu errado. Tente novamente.")
+
     elif escolha_usuario == 3:
-        soma_precos = 0
-    for item in estoque:
+        def somar_total():
+            total_geral = 0
+            for chave in estoque:
+                total_geral += chave["preço"] * chave["quantidade"]
+            print(f"Total geral do estoque: {total_geral}")
         
-        soma_precos = soma_precos + float(item["preço"])
-    
-    print(f"Soma de todos os preços: R$ {soma_precos:.2f}")
+        somar_total()  
 
     elif escolha_usuario == 4:
-    soma_quantidades = 0
-    for item in estoque:
+        def somar_quantidade_total():
+            total_geral = 0
+            for chave in estoque:
+                total_geral += chave["quantidade"]
+            print(f"Total geral do estoque: {total_geral}")
         
-        soma_quantidades = soma_quantidades + int(item["quantidade"])
-        
-    print(f"Total de itens no estoque: {soma_quantidades}")
+        somar_quantidade_total()
 
-    elif escolha_usuario == 0:
-    print("Obrigado por usar o nosso sistema.")
-    break
-    
+    elif escolha_usuario == 5:
+        def buscar_produto():
+            busca = input("Digite o produto buscado: ")  
 
+            for chave in estoque:
+                if chave ["tipo"] == busca:
+                    print(chave["quantidade"])
+                    print(chave["preço"])
+        buscar_produto()
 
+    elif escolha_usuario == 0:  
+        print("Obrigado por usar o nosso sistema.")
+        break
